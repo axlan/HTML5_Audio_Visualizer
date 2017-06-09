f754688f7d04e72b341ec2795ae9e335aa63c064
axlan <feros32@gmail.com>
2017-06-08 18:01:58 -0700

    Adapted script to take output from matlab scripts

---
+++ js/html5_post_visualizer.js
@@ -0,0 +1,219 @@
+/**
+ * An audio spectrum visualizer built with HTML5 Audio API
+ * Author:Wayou
+ * License: MIT
+ * Feb 15, 2014
+ */
+window.onload = function() {
+    new Visualizer().ini();
+};
+var Visualizer = function() {
+    this.file = null; //the current file
+    this.fileName = null; //the current file name
+    this.audioContext = null;
+    this.source = null; //the audio source
+    this.info = document.getElementById('info').innerHTML; //used to upgrade the UI information
+    this.infoUpdateId = null; //to store the setTimeout ID and clear the interval
+    this.animationId = null;
+    this.status = 1; //flag for sound is playing 1 or stopped 0
+    this.forceStop = false;
+    this.allCapsReachBottom = false;
+	this.start_time = null;
+	this.data_idx = null;
+	this.data = null;
+	this.times = null;
+};
+Visualizer.prototype = {
+    ini: function() {
+        this._prepareAPI();
+        this._addEventListner();
+    },
+    _prepareAPI: function() {
+        //fix browser vender for AudioContext and requestAnimationFrame
+        window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.msAudioContext;
+        window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame;
+        window.cancelAnimationFrame = window.cancelAnimationFrame || window.webkitCancelAnimationFrame || window.mozCancelAnimationFrame || window.msCancelAnimationFrame;
+        try {
+            this.audioContext = new AudioContext();
+        } catch (e) {
+            this._updateInfo('!Your browser does not support AudioContext', false);
+            console.log(e);
+        }
+    },
+    _addEventListner: function() {
+        var that = this,
+            audioInput = document.getElementById('uploadedFile'),
+            dropContainer = document.getElementsByTagName("canvas")[0];
+        //listen the file upload
+        audioInput.onchange = function() {
+            if (that.audioContext===null) {return;};
+
+            //the if statement fixes the file selction cancle, because the onchange will trigger even the file selection been canceled
+            if (audioInput.files.length !== 0) {
+                //only process the first file
+                that.file = audioInput.files[0];
+                that.fileName = that.file.name;
+                if (that.status === 1) {
+                    //the sound is still playing but we upload another file, so set the forceStop flag to true
+                    that.forceStop = true;
+                };
+                document.getElementById('fileWrapper').style.opacity = 1;
+                that._updateInfo('Uploading', true);
+                //once the file is ready,start the visualizer
+                that._start();
+            };
+        };
+        //listen the drag & drop
+        dropContainer.addEventListener("dragenter", function() {
+            document.getElementById('fileWrapper').style.opacity = 1;
+            that._updateInfo('Drop it on the page', true);
+        }, false);
+        dropContainer.addEventListener("dragover", function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+            //set the drop mode
+            e.dataTransfer.dropEffect = 'copy';
+        }, false);
+        dropContainer.addEventListener("dragleave", function() {
+            document.getElementById('fileWrapper').style.opacity = 0.2;
+            that._updateInfo(that.info, false);
+        }, false);
+        dropContainer.addEventListener("drop", function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+            if (that.audioContext===null) {return;};
+            document.getElementById('fileWrapper').style.opacity = 1;
+            that._updateInfo('Uploading', true);
+            //get the dropped file
+            that.file = e.dataTransfer.files[0];
+            if (that.status === 1) {
+                document.getElementById('fileWrapper').style.opacity = 1;
+                that.forceStop = true;
+            };
+            that.fileName = that.file.name;
+            //once the file is ready,start the visualizer
+            that._start();
+        }, false);
+    },
+    _start: function() {
+        //read and decode the file into audio array buffer
+        var that = this,
+            file = this.file,
+            fr = new FileReader();
+        fr.onload = function(e) {
+            var fileResult = e.target.result;
+			var lines = fileResult.split('\n');
+			that.times = []
+			that.data = []
+			for(var i = 0;i < lines.length;i++){
+				items = lines[i].split(',');
+				that.times[i] = parseFloat(items[0]);
+				that.data[i] = []
+				for(var j = 1;j < items.length;j++){
+					that.data[i][j-1] = parseInt(items[j]);
+				}
+			}
+            that._updateInfo('Decode succussfully,start the visualizer', true);
+			that._visualize();
+        };
+        fr.onerror = function(e) {
+            that._updateInfo('!Fail to read the file', false);
+            console.error(e);
+        };
+        //assign the file to the reader
+        this._updateInfo('Starting read the file', true);
+        fr.readAsText(file);
+    },
+    _visualize: function() {
+        this._updateInfo('Playing ' + this.fileName, false);
+        this.info = 'Playing ' + this.fileName;
+        document.getElementById('fileWrapper').style.opacity = 0.2;
+        this._drawSpectrum();
+    },
+    _drawSpectrum: function() {
+        var that = this,
+            canvas = document.getElementById('canvas'),
+            cwidth = canvas.width,
+            cheight = canvas.height - 2,
+            meterWidth = 10, //width of the meters in the spectrum
+            gap = 2, //gap between meters
+            capHeight = 2,
+            capStyle = '#fff',
+            meterNum = that.data[0].length, //count of the meters
+            capYPositionArray = []; ////store the vertical position of hte caps for the preivous frame
+        ctx = canvas.getContext('2d'),
+        gradient = ctx.createLinearGradient(0, 0, 0, 300);
+        gradient.addColorStop(1, '#0f0');
+        gradient.addColorStop(0.5, '#ff0');
+        gradient.addColorStop(0, '#f00');
+		that.start_time = new Date().getTime();
+		that.data_idx = 0;
+        var drawMeter = function() {
+			if (that.data_idx + 1 > that.data.length) {
+				cancelAnimationFrame(that.animationId); //since the sound is stoped and animation finished, stop the requestAnimation to prevent potential memory leak,THIS IS VERY IMPORTANT!
+                return;
+			}
+			var duration = (new Date().getTime() - that.start_time)/1000;
+			if (that.times[that.data_idx + 1] > duration) {
+				that.animationId = requestAnimationFrame(drawMeter);
+				return;
+			}
+            var array = that.data[that.data_idx];
+            var step = Math.round(array.length / meterNum); //sample limited data from the total array
+            ctx.clearRect(0, 0, cwidth, cheight);
+            for (var i = 0; i < meterNum; i++) {
+                var value = array[i * step];
+                if (capYPositionArray.length < Math.round(meterNum)) {
+                    capYPositionArray.push(value);
+                };
+                ctx.fillStyle = capStyle;
+                //draw the cap, with transition effect
+                if (value < capYPositionArray[i]) {
+                    ctx.fillRect(i * 12, cheight - (--capYPositionArray[i]), meterWidth, capHeight);
+                } else {
+                    ctx.fillRect(i * 12, cheight - value, meterWidth, capHeight);
+                    capYPositionArray[i] = value;
+                };
+                ctx.fillStyle = gradient; //set the filllStyle to gradient for a better look
+                ctx.fillRect(i * 12 /*meterWidth+gap*/ , cheight - value + capHeight, meterWidth, cheight); //the meter
+            }
+			that.data_idx++;
+            that.animationId = requestAnimationFrame(drawMeter);
+        }
+        this.animationId = requestAnimationFrame(drawMeter);
+    },
+    _audioEnd: function(instance) {
+        if (this.forceStop) {
+            this.forceStop = false;
+            this.status = 1;
+            return;
+        };
+        this.status = 0;
+        var text = 'HTML5 Audio API showcase | An Audio Viusalizer';
+        document.getElementById('fileWrapper').style.opacity = 1;
+        document.getElementById('info').innerHTML = text;
+        instance.info = text;
+        document.getElementById('uploadedFile').value = '';
+    },
+    _updateInfo: function(text, processing) {
+        var infoBar = document.getElementById('info'),
+            dots = '...',
+            i = 0,
+            that = this;
+        infoBar.innerHTML = text + dots.substring(0, i++);
+        if (this.infoUpdateId !== null) {
+            clearTimeout(this.infoUpdateId);
+        };
+        if (processing) {
+            //animate dots at the end of the info text
+            var animateDot = function() {
+                if (i > 3) {
+                    i = 0
+                };
+                infoBar.innerHTML = text + dots.substring(0, i++);
+                that.infoUpdateId = setTimeout(animateDot, 250);
+            }
+            this.infoUpdateId = setTimeout(animateDot, 250);
+        };
+    }
+}
